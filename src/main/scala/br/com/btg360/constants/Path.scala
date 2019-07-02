9c64db21de1d327d4f215011f0610058d8138753
jurandi <jurandi.co@gmail.com>
2019-07-02 19:35:28 -0300

    Cluster application fitness - part 5

--- src/main/scala/br/com/btg360/constants/Path.scala
+++ src/main/scala/br/com/btg360/constants/Path.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 object Path {
 
-  val PROJECT: String = new File("").getAbsolutePath
+  val PROJECT: String = new File(".").getCanonicalPath
 
   val STORAGE: String = "%s/storage".format(PROJECT)
 
