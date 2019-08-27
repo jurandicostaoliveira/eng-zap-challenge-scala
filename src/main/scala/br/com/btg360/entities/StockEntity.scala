6496900e09503358f87dee06f154a082be4443e5
jurandi <jurandi.co@gmail.com>
2019-08-27 15:14:26 -0300

    Insert multiple data with prepare statement - part 2

--- src/main/scala/br/com/btg360/entities/StockEntity.scala
+++ src/main/scala/br/com/btg360/entities/StockEntity.scala
@@ -1,7 +1,6 @@
 package br.com.btg360.entities
 
 import br.com.btg360.application.Entity
-import br.com.btg360.constants.CustomEscape
 
 import scala.collection.Map
 import scala.collection.mutable.HashMap
@@ -31,7 +30,7 @@ case class StockEntity(
              product: ProductEntity
            ): HashMap[String, Any] = {
 
-    val productName = CustomEscape.toJson(product.productName)
+    val productName = product.productName
     HashMap(
       "userId" -> consolidated.userId,
       "userSent" -> consolidated.userSent,
@@ -41,50 +40,50 @@ case class StockEntity(
       "platformId" -> consolidated.platformId,
       "createdAt" -> consolidated.createdAt,
       //
-      "attribute1" -> CustomEscape.toJson(product.attribute1),
-      "attribute2" -> CustomEscape.toJson(product.attribute2),
-      "attribute3" -> CustomEscape.toJson(product.attribute3),
-      "attribute4" -> CustomEscape.toJson(product.attribute4),
-      "attribute5" -> CustomEscape.toJson(product.attribute5),
-      "attribute6" -> CustomEscape.toJson(product.attribute6),
-      "attribute7" -> CustomEscape.toJson(product.attribute7),
-      "attribute8" -> CustomEscape.toJson(product.attribute8),
-      "attribute9" -> CustomEscape.toJson(product.attribute9),
-      "attribute10" -> CustomEscape.toJson(product.attribute10),
-      "attribute11" -> CustomEscape.toJson(product.attribute11),
-      "attribute12" -> CustomEscape.toJson(product.attribute12),
-      "attribute13" -> CustomEscape.toJson(product.attribute13),
-      "attribute14" -> CustomEscape.toJson(product.attribute14),
-      "attribute15" -> CustomEscape.toJson(product.attribute15),
+      "attribute1" -> product.attribute1,
+      "attribute2" -> product.attribute2,
+      "attribute3" -> product.attribute3,
+      "attribute4" -> product.attribute4,
+      "attribute5" -> product.attribute5,
+      "attribute6" -> product.attribute6,
+      "attribute7" -> product.attribute7,
+      "attribute8" -> product.attribute8,
+      "attribute9" -> product.attribute9,
+      "attribute10" -> product.attribute10,
+      "attribute11" -> product.attribute11,
+      "attribute12" -> product.attribute12,
+      "attribute13" -> product.attribute13,
+      "attribute14" -> product.attribute14,
+      "attribute15" -> product.attribute15,
       "availability" -> product.availability,
       "barCode" -> product.barCode,
-      "brand" -> CustomEscape.toJson(product.brand),
-      "category" -> CustomEscape.toJson(product.category),
-      "city" -> CustomEscape.toJson(product.city),
-      "condition" -> CustomEscape.toJson(product.condition),
-      "country" -> CustomEscape.toJson(product.country),
-      "establishment" -> CustomEscape.toJson(product.establishment),
-      "establishmentLink" -> CustomEscape.toJson(product.establishmentLink),
+      "brand" -> product.brand,
+      "category" -> product.category,
+      "city" -> product.city,
+      "condition" -> product.condition,
+      "country" -> product.country,
+      "establishment" -> product.establishment,
+      "establishmentLink" -> product.establishmentLink,
       "finalDate" -> product.finalDate,
-      "image" -> CustomEscape.toJson(product.image),
+      "image" -> product.image,
       "initDate" -> product.initDate,
       "installment" -> product.installment,
       "installmentDesc" -> product.installmentDesc,
       "installmentValue" -> product.installmentValue,
-      "otherOffersLink" -> CustomEscape.toJson(product.otherOffersLink),
-      "productId" -> CustomEscape.toJson(product.productId),
-      "price" -> CustomEscape.toJson(product.price),
-      "priceBy" -> CustomEscape.toJson(product.priceBy),
+      "otherOffersLink" -> product.otherOffersLink,
+      "productId" -> product.productId,
+      "price" -> product.price,
+      "priceBy" -> product.priceBy,
       "priceNum" -> product.priceNum,
       "priceByNum" -> product.priceByNum,
       "productDesc" -> productName, //Hack .: productDesc
       "productGroupId" -> product.productGroupId,
-      "productLink" -> CustomEscape.toJson(product.productLink),
+      "productLink" -> product.productLink,
       "productName" -> productName,
       "quantity" -> product.quantity,
       "state" -> product.state,
-      "subCategory" -> CustomEscape.toJson(product.subCategory),
-      "subCategoryTwo" -> CustomEscape.toJson(product.subCategoryTwo),
+      "subCategory" -> product.subCategory,
+      "subCategoryTwo" -> product.subCategoryTwo,
       "totalQtySold" -> product.totalQtySold
     )
   }
