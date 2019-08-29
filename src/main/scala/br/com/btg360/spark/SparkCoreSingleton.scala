4b117cce00770b2d71fbfe270b8fb3a3ca51beb3
jurandi <jurandi.co@gmail.com>
2019-08-29 14:06:25 -0300

    Implement scheduler mode fair

--- src/main/scala/br/com/btg360/spark/SparkCoreSingleton.scala
+++ src/main/scala/br/com/btg360/spark/SparkCoreSingleton.scala
@@ -15,45 +15,13 @@ object SparkCoreSingleton extends Serializable {
 
   private val fileName = "cassandra-%s.properties".format(Environment.getAppEnv)
 
-  //  /**
-  //    * Return the settings
-  //    *
-  //    * @return SparkConf
-  //    */
-  //  private def getConf: SparkConf = {
-  //    val p: Properties = new PropService().get(fileName)
-  //    val sparkConf = new SparkConf().setMaster(p.getProperty("master"))
-  //      .setAppName(p.getProperty("appName"))
-  //      .set("spark.cassandra.connection.host", p.getProperty("host"))
-  //      .set("spark.cassandra.auth.username", p.getProperty("username"))
-  //      .set("spark.cassandra.auth.password", p.getProperty("password"))
-  //      .set("spark.driver.memory", p.getProperty("sparkDriverMemory"))
-  //      .set("spark.executor.memory", p.getProperty("sparkExecutorMemory"))
-  //      .set("spark.driver.allowMultipleContexts", p.getProperty("sparkDriverAllowMultipleContexts"))
-  //      .set("spark.cassandra.connection.compression", p.getProperty("sparkCassandraConnectionCompression"))
-  //      .set("spark.ui.port", p.getProperty("sparkUiPort"))
-  //      .set("spark.cassandra.connection.timeout_ms", p.getProperty("sparkCassandraConnectionTimeoutMs"))
-  //      .set("spark.network.timeout", p.getProperty("sparkNetworkTimeout"))
-  //      .set("spark.cassandra.input.consistency.level", p.getProperty("sparkCassandraInputConsistencyLevel"))
-  //
-  //    if (Environment.isDevelopment) {
-  //      sparkConf.set("spark.local.dir", p.getProperty("sparkLocalDir"))
-  //    }
-  //
-  //    sparkConf
-  //  }
-
   /**
     *
     * @return SparkSession
     */
   private def getSparkSession: SparkSession = {
-    val p: Properties = new PropService().get(fileName)
+    val p: Properties = new PropService().get(this.fileName)
     val builder: SparkSession.Builder = SparkSession.builder()
-
-    if (Environment.isDevelopment) {
-      //builder.config("spark.local.dir", p.getProperty("sparkLocalDir"))
-    }
 
     return builder.master(p.getProperty("master"))
       .appName(p.getProperty("appName"))
@@ -70,6 +38,8 @@ object SparkCoreSingleton extends Serializable {
       .config("spark.network.timeout", p.getProperty("sparkNetworkTimeout"))
       .config("spark.cassandra.input.consistency.level", p.getProperty("sparkCassandraInputConsistencyLevel"))
       .config("spark.local.dir", p.getProperty("sparkLocalDir"))
+      .config("spark.scheduler.mode", p.getProperty("sparkSchedulerMode"))
+      .config("spark.scheduler.allocation.file", p.getProperty("sparkSchedulerAllocationFile"))
       .getOrCreate()
   }
 
@@ -79,10 +49,11 @@ object SparkCoreSingleton extends Serializable {
     * @return
     */
   def getContext: SparkContext = this.synchronized {
-    if (sparkContext == null) {
-      sparkContext = this.getSparkSession.sparkContext
+    if (this.sparkContext == null) {
+      this.sparkContext = this.getSparkSession.sparkContext
+      this.sparkContext.setLocalProperty("spark.scheduler.pool", "default")
     }
-    sparkContext
+    this.sparkContext
   }
 
   /**
