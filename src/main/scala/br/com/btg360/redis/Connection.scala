1567d01203df4d577e1f3a9bc9ff0df12281e2c8
jurandi <jurandi.co@gmail.com>
2019-10-30 10:43:40 -0300

    Migration from redis to dedicated machine

--- src/main/scala/br/com/btg360/redis/Connection.scala
+++ src/main/scala/br/com/btg360/redis/Connection.scala
@@ -4,6 +4,8 @@ import br.com.btg360.constants.Environment
 import br.com.btg360.services.PropService
 import com.redis._
 
+import scala.util.Try
+
 class Connection {
 
   private val fileName = "redis-%s.properties".format(Environment.getAppEnv)
@@ -11,7 +13,12 @@ class Connection {
   def get: RedisClient = {
     try {
       val p = new PropService().get(fileName)
-      new RedisClient(p.getProperty("host"), p.getProperty("port").toInt, p.getProperty("database").toInt)
+      new RedisClient(
+        p.getProperty("host"),
+        p.getProperty("port").toInt,
+        p.getProperty("database").toInt,
+        Try(p.getProperty("secret")).toOption
+      )
     } catch {
       case e: Exception => println("REDIS CONNECTION ERROR: " + e.printStackTrace())
         new RedisClient()
