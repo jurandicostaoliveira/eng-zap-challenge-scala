c9d08c48028553a69270c6f523336ef86c843a15
jurandi <jurandi.co@gmail.com>
2020-04-06 11:11:01 -0300

    Twig to url encode

--- src/main/scala/br/com/btg360/constants/Twig.scala
+++ src/main/scala/br/com/btg360/constants/Twig.scala
@@ -12,4 +12,23 @@ object Twig {
     "(\\{\\{)(.*?)(\\}\\})".r.findFirstIn(value).nonEmpty
   }
 
+  /**
+    * Replace url encode for twig tags
+    *
+    * @param String value
+    * @return String
+    */
+  def toUrlEncode(value: String): String = {
+    val patterns: Map[String, String] = Map(
+      "%7B%7B" -> "{{", "%20" -> "", "%7D%7D" -> "}}"
+    )
+
+    var replacement: String = value
+    for ((key, value) <- patterns) {
+      replacement = replacement.replaceAll(key, value)
+    }
+
+    replacement
+  }
+
 }
